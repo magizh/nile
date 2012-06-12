7c8c485a024d8d49666a979751386f0e5e491c78
Magi <mahilis@live.com>
2012-06-11 19:54:12 -0700

    first

---
+++ nile/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nile::Application.config.secret_token = '287e295324b157f0c8589ddd1da49a7b311a8b956021905df159162883680f9bb550094d8d27b8ee70c0d25cc7433c219394fc2dfce653552b7dd25e61bfeeca'
